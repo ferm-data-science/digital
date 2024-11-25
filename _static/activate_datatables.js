36406fa017a752316e83e59d8dde38f998474822
nsitala <nsitala@users.noreply.github.com>
2024-11-25 07:47:09 +0000

    deploy: b08fe8f48d62672f86661667a859299af8e6c3ea

---
+++ _static/activate_datatables.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2023 Varun Sharma
+//
+// SPDX-License-Identifier: MIT
+
+$(document).ready( function () {
+    $('table.sphinx-datatable').DataTable(
+        {
+        },
+
+    );
+} );
